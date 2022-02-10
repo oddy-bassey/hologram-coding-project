356fcedceb610af4bf72e71b95d37ec70b57f2d6
oddy-bassey <oddybassey@gmail.com>
2022-02-10 12:02:06 +0100

    solved: distinct ways to make totalCents

--- src/main/java/com/hologramsciences/Algorithms.java
+++ src/main/java/com/hologramsciences/Algorithms.java
@@ -40,7 +40,7 @@ public class Algorithms {
 
         if(listOfLists.size()<1) return Collections.emptyList();
 
-        // cartesian product given by A * B = {{a, b} : a E A, b E B}
+        // Solution: cartesian product given by A * B = {{a, b} : a E A, b E B}
         List<List<T>> result = new ArrayList<>();
 
         // Assuming only two lists are given
@@ -49,9 +49,12 @@ public class Algorithms {
 
             // iterating through the second list
             for (T b : listOfLists.get(1)) {
+                // determining {a, b}
                 result.add(Arrays.asList(a, b));
             }
         }
+
+        // returning the result of the cartesian product of a list of lists of any type T
         return result;
     }
 
@@ -64,6 +67,25 @@ public class Algorithms {
      */
     public static final long countNumWaysMakeChange(final int totalCents) {
         // TODO Implement me
-        return 0L;
+
+        // list of United States coins
+        List<Integer> coins = Arrays.asList(1, 5, 10, 25, 50, 100);
+
+        // Array of possible combinations (distinct ways)
+        int [] combinations = new int[totalCents+1];
+
+        combinations[0] = 1;
+
+        // Iterating through each coin to determine all possible combinations
+        for(int coin : coins){
+            for(int i=1; i < combinations.length; i++){
+                if(i>=coin){
+                    combinations[i] += combinations[i-coin];
+                }
+            }
+        }
+
+        // returning the number of combinations (distinct ways) to make totalCents ${totalCents}
+        return combinations[totalCents];
     }
 }
